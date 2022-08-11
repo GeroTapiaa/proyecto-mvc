2033ca8242f57a35a86e00846bf5d48d32216182
GeroTapiaa <gerook98@hotmail.com>
2022-08-10 22:01:24 -0300

    mvc done

--- mi-portfolio-mvc/routes/main.js
+++ mi-portfolio-mvc/routes/main.js
@@ -6,8 +6,9 @@ const router = express.Router()
 const {home , about} = require("../controllers/mainController")
 /* "/" */
 
-app.get("/", home)
-app.get("/about", about)
+router
+    .get("/", home)
+    .get("/about", about)
 
 
 module.exports = router
